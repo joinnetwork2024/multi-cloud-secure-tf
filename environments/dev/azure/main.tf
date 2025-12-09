4dbb620bdf3d136c8302cc1281bcf8f926af54cf
joinnetwork2024 <yw.chan@hotmail.com>
2025-12-09 00:54:59 +0000

    test3rd

--- environments/dev/azure/main.tf
+++ environments/dev/azure/main.tf
@@ -28,8 +28,8 @@ resource "azurerm_storage_account" "secure" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  public_network_access_enabled     = false
-  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = ture
+  infrastructure_encryption_enabled = false
 
   min_tls_version = "TLS1_2"
 
