ad58810e8d92d7e2f8d90696e125006a00e844fe
joinnetwork2024 <yw.chan@hotmail.com>
2025-12-16 23:09:01 +0000

    opa

--- environments/dev/aws/aws_variables.tf
+++ environments/dev/aws/aws_variables.tf
@@ -1,22 +1,22 @@
-# variable "aws_region" {
-#   description = "The AWS region to deploy resources into."
-#   type        = string
-# }
+variable "aws_region" {
+  description = "The AWS region to deploy resources into."
+  type        = string
+}
 
-# variable "project_name" {
-#   description = "A unique prefix for all resources."
-#   type        = string
-#   default     = "mcstf-jojo" # Multi-Cloud Secure Terraform
-# }
+variable "project_name" {
+  description = "A unique prefix for all resources."
+  type        = string
+  default     = "mcstf-jojo" # Multi-Cloud Secure Terraform
+}
 
-# variable "vault_addr" {
-#   description = "Vault address"
-#   type        = string
-# }
+variable "vault_addr" {
+  description = "Vault address"
+  type        = string
+}
 
-# variable "vaultroleid" {}
+variable "vaultroleid" {}
 
-# variable "vaultsecretid" {}
+variable "vaultsecretid" {}
 
 variable "vpc_cidr" {
   description = "VPC CIDR"
